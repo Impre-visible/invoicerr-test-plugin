3c99d2aab45f15065a56732ce5b544d9b5ec5b42
Impre'visible <69050895+Impre-visible@users.noreply.github.com>
2025-07-25 13:27:55 +0200

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,14 +1,16 @@
-const PLUGIN_DESCRIPTION = {
-  name: "Factur-X"
-  description: "This plugin is only used to test the plugin feature of invoicerr"
-  
-  pdf_format: {
-    format_name = "Factur-X (Plugin)" 
-    format_key = "factur-x-plugin" // A unique key used to identify the format, make it as unique as possible
-    format_function = null // If null the code we'll use the "pdf_format" function, else it'll use the function used
-  }
-}
+export default class TestPlugin {
+  name = "Factur-X";
+  description = "This plugin is only used to test the plugin feature of invoicerr";
 
-function pdf_format(invoice) {
-  return Buffer.from(invoice.exportXml('facturx'), 'utf-8); // Simply export the factur-x thing
+  pdf_format_info() {
+    return {
+      format_name: "Factur-X (Plugin)",
+      format_key: "factur-x-plugin", // A unique key used to identify the format, make it as unique as possible
+      format_function: null, // If null the code will use the "pdf_format" function, else it'll use the named function
+    };
+  }
+
+  pdf_format(invoice) {
+    return Buffer.from(invoice.exportXml('facturx'), 'utf-8'); // Simply export the factur-x thing
+  }
 }
