54966e9dcb233390f8b59e8008225ac4f61cbfcb
Impre'visible <69050895+Impre-visible@users.noreply.github.com>
2025-07-25 13:28:04 +0200

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -6,7 +6,6 @@ export default class TestPlugin {
     return {
       format_name: "Factur-X (Plugin)",
       format_key: "factur-x-plugin", // A unique key used to identify the format, make it as unique as possible
-      format_function: null, // If null the code will use the "pdf_format" function, else it'll use the named function
     };
   }
 
